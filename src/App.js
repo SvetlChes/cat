216fa0cb6e55842a8a16f4533b128eb6b21df790
SvetlChes <119002605+SvetlChes@users.noreply.github.com>
2022-12-06 18:23:21 +0000

    pdz 1

--- src/App.js
+++ src/App.js
@@ -3,25 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h1>Hello,React</h1>
+      <p>Светлана Чеснокова<br/>
+        8-919-259-61-94<br/>
+        svetlana2021chesnokova@gmail.com
+      </p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSldjUO_vbSs0Tpr-3oFzKoUvS1wY1zUQyL01IDwN6nNQ&s"/>
+      <p>Липецк<br/>
+         Россия<br/>
+         население около 500 000 человек<br/>
+      </p>
+      <img src="https://www.dorogavrim.ru/img/lib/flag_obl_lipeckaya.jpg"/>
     </div>
   );
 }
